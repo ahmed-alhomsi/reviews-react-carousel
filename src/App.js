43a6ead2b1805b3b34988745b1fc8bfc45194141
ahmed-alhomsi <ahmed.alhomsi.official@gmail.com>
2022-04-18 18:45:43 +0300

    a slider that automatically slides contact after 3 seconds, from fcc jogn smilga 15 react projects video.

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import data from "./data"
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+
+const App = () => {
+  const [people, setpeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if(index < 0) {
+      setIndex(people.length - 1);
+    }
+
+    if(index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+
+    return ()=> {
+      clearInterval(slider);
+    }
+  }, [index])
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <section className='section-center'>
+        {
+          people.map((person, persIndex) => {
+            const {id, name, image, title, quote} = person;
+            let position = 'nextSlide';
+
+            if(persIndex === index) {
+              position = 'activeSlide';
+            }
+
+            if(persIndex === index - 1 || (index === 0 && persIndex === people.length - 1)) {
+              position = 'prevSlide';
+            }
+            
+            return (
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+              </article>
+            )
+          })
+        }
+
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </section>
+    </section>
+  )
+}
+
+export default App
